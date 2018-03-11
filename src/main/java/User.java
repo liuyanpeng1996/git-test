4138d16cbc8a6b4c818f45ccde15bebcc04b3b87
liuyanpeng1996 <1377191204@qq.com>
2018-03-11 14:14:46 +0800

    add name

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,7 @@
 public class User {
 
     private Integer id;
+    private String name;
 
     public User(){
 
@@ -12,5 +13,13 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
